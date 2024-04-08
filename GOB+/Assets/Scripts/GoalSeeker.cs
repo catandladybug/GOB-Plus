04e5d78c22a9dbd95f9ccd1caaf2d6507a713a35
catandladybug <98188215+catandladybug@users.noreply.github.com>
2024-04-07 20:03:31 -0700

    upload

--- GOB+/Assets/Scripts/GoalSeeker.cs
+++ GOB+/Assets/Scripts/GoalSeeker.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GoalSeeker : MonoBehaviour
 {
@@ -8,49 +10,50 @@ public class GoalSeeker : MonoBehaviour
     Action[] mActions;
     Action mChangeOverTime;
     const float TICK_LENGTH = 5.0f;
+    public TextMeshProUGUI display;
 
     void Start()
     {
         mGoals = new Goal[3];
-        mGoals[0] = new Goal("Eat", 4);
-        mGoals[1] = new Goal("Sleep", 3);
-        mGoals[2] = new Goal("Bathroom", 3);
+        mGoals[0] = new Goal("Mac and Cheese", 4);
+        mGoals[1] = new Goal("Salad", 3);
+        mGoals[2] = new Goal("Tomato Soup", 3);
 
         mActions = new Action[6];
-        mActions[0] = new Action("eat some raw food");
-        mActions[0].targetGoals.Add(new Goal("Eat", -3f));
-        mActions[0].targetGoals.Add(new Goal("Sleep", +2f));
-        mActions[0].targetGoals.Add(new Goal("Bathroom", +1f));
+        mActions[0] = new Action("serve mac and cheese");
+        mActions[0].targetGoals.Add(new Goal("Mac and Cheese", -3f));
+        mActions[0].targetGoals.Add(new Goal("Salad", +2f));
+        mActions[0].targetGoals.Add(new Goal("Tomato Soup", +1f));
 
-        mActions[1] = new Action("eat a snack");
-        mActions[1].targetGoals.Add(new Goal("Eat", -2f));
-        mActions[1].targetGoals.Add(new Goal("Sleep", -1f));
-        mActions[1].targetGoals.Add(new Goal("Bathroom", +1f));
+        mActions[1] = new Action("serve mac and cheese");
+        mActions[1].targetGoals.Add(new Goal("Mac and Cheese", -2f));
+        mActions[1].targetGoals.Add(new Goal("Salad", +1f));
+        mActions[1].targetGoals.Add(new Goal("Tomato Soup", +1f));
 
-        mActions[2] = new Action("sleep in the bed");
-        mActions[2].targetGoals.Add(new Goal("Eat", +2f));
-        mActions[2].targetGoals.Add(new Goal("Sleep", -4f));
-        mActions[2].targetGoals.Add(new Goal("Bathroom", +2f));
+        mActions[2] = new Action("serve salad");
+        mActions[2].targetGoals.Add(new Goal("Mac and Cheese", +2f));
+        mActions[2].targetGoals.Add(new Goal("Salad", -4f));
+        mActions[2].targetGoals.Add(new Goal("Tomato Soup", +2f));
 
-        mActions[3] = new Action("sleep on the sofa");
-        mActions[3].targetGoals.Add(new Goal("Eat", +1f));
-        mActions[3].targetGoals.Add(new Goal("Sleep", -2f));
-        mActions[3].targetGoals.Add(new Goal("Bathroom", +1f));
+        mActions[3] = new Action("serve salad");
+        mActions[3].targetGoals.Add(new Goal("Mac and Cheese", +1f));
+        mActions[3].targetGoals.Add(new Goal("Salad", -2f));
+        mActions[3].targetGoals.Add(new Goal("Tomato Soup", +1f));
 
-        mActions[4] = new Action("drink a soda");
-        mActions[4].targetGoals.Add(new Goal("Eat", -1f));
-        mActions[4].targetGoals.Add(new Goal("Sleep", -2f));
-        mActions[4].targetGoals.Add(new Goal("Bathroom", +3f));
+        mActions[4] = new Action("serve tomato soup");
+        mActions[4].targetGoals.Add(new Goal("Mac and Cheese", +1f));
+        mActions[4].targetGoals.Add(new Goal("Salad", +2f));
+        mActions[4].targetGoals.Add(new Goal("Tomato Soup", -3f));
 
-        mActions[5] = new Action("visit the bathroom");
-        mActions[5].targetGoals.Add(new Goal("Eat", 0f));
-        mActions[5].targetGoals.Add(new Goal("Sleep", 0f));
-        mActions[5].targetGoals.Add(new Goal("Bathroom", -4f));
+        mActions[5] = new Action("serve tomato soup");
+        mActions[5].targetGoals.Add(new Goal("Mac and Cheese", 0f));
+        mActions[5].targetGoals.Add(new Goal("Salad", 0f));
+        mActions[5].targetGoals.Add(new Goal("Tomato Soup", -4f));
 
         mChangeOverTime = new Action("tick");
-        mChangeOverTime.targetGoals.Add(new Goal("Eat", +4f));
-        mChangeOverTime.targetGoals.Add(new Goal("Sleep", +1f));
-        mChangeOverTime.targetGoals.Add(new Goal("Bathroom", +2f));
+        mChangeOverTime.targetGoals.Add(new Goal("Mac and Cheese", +2f));
+        mChangeOverTime.targetGoals.Add(new Goal("Salad", +1f));
+        mChangeOverTime.targetGoals.Add(new Goal("Tomato Soup", +2f));
 
         Debug.Log("Starting. One hour passes every " + TICK_LENGTH + " seconds.");
         InvokeRepeating("Tick", 0f, TICK_LENGTH);
@@ -65,19 +68,17 @@ public class GoalSeeker : MonoBehaviour
             goal.value += mChangeOverTime.GetGoalChange(goal);
             goal.value = Mathf.Max(goal.value, 0);
         }
-
-        //PrintGoals();
     }
 
     void PrintGoals()
     {
-        string goalString = "";
+        string goalString = "# of customers who want: " + "\n";
         foreach (Goal goal in mGoals)
         {
-            goalString += goal.name + ": " + goal.value + "; ";
+            goalString += goal.name + ": " + goal.value + "\n";
         }
-        goalString += "Discontentment: " + CurrentDiscontentment();
-        Debug.Log(goalString);
+        goalString += "Total Customers: " + CurrentDiscontentment();
+        display.text = goalString;
     }
 
     void Update()
@@ -86,7 +87,7 @@ public class GoalSeeker : MonoBehaviour
         {
 
             Action bestThingToDo = ChooseAction(mActions, mGoals);
-            Debug.Log("I think I will " + bestThingToDo.name);
+            display.text = "I think I will " + bestThingToDo.name;
 
             foreach (Goal goal in mGoals)
             {
@@ -94,13 +95,13 @@ public class GoalSeeker : MonoBehaviour
                 goal.value = Mathf.Max(goal.value, 0);
             }
 
-            Debug.Log("New Stats: ");
-            PrintGoals();
+            //Debug.Log("New Stats: ");
+            //PrintGoals();
         }
 
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            Debug.Log("Current Stats: ");
+            Debug.Log("# of customers who want: ");
             PrintGoals();
         }
     }
@@ -143,7 +144,7 @@ public class GoalSeeker : MonoBehaviour
         float total = 0f;
         foreach (Goal goal in mGoals)
         {
-            total += (goal.value * goal.value);
+            total += (goal.value);
         }
         return total;
     }
